b17fa120d61d0fc1a6317b9e2f241b4a4d4b39b5
Utkarsh Jain <86288552+Utkarsh-rafay@users.noreply.github.com>
2021-08-04 17:49:03 +0530

    Update variables.tf

--- rafay_eks_cluster/variables.tf
+++ rafay_eks_cluster/variables.tf
@@ -1,14 +1,17 @@
 variable "name" {
   description = "name to pass to cluster name tag"
+  type = string
   default = "rauto-infra-provisioner"
 }
 
 variable "yamlfilepath" {
   description = "path to cluster yaml file"
+  type = string
   default = "eks-cluster.yaml"
 }
 
 variable "project" {
   description = "project to pass to project tag"
+  type = string
   default = "defaultproject"
 }
