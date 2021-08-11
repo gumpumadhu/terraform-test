d4e002469bb4e2ea33ad0d9b960a13c87acd43f2
Utkarsh Jain <86288552+Utkarsh-rafay@users.noreply.github.com>
2021-08-11 10:26:39 +0530

    Create rafay_eks.sh

---
+++ rafay_eks_cluster/rafay_eks.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+RCTL_FILE="rctl-linux-amd64.tar.bz2"
+#RCTL_URL="https://rafay-prod-cli.s3-us-west-2.amazonaws.com/publish/rctl-linux-amd64.tar.bz2"
+RCTL_URL="https://rctl.s3-us-west-1.amazonaws.com/origin/master-283/rctl-linux-amd64.tar.bz2"
+
+wget $RCTL_URL
+if [ $? -eq 0 ];
+then
+    echo "[+] Successfully Downloaded RCTL binary"
+fi
+tar -xvf $RCTL_FILE
+
+#Check rctl version
+./rctl version
+
+#Clone spec repo
+GIT_URL=$(echo $REPO_URL | sed "s/https:\/\//https:\/\/$REPO_USER:$REPO_PASSWORD@/")
+git clone $GIT_URL
+if [ $? -ne 0 ];
+then
+    echo "[+] Failed to clone repo: $REPO_URL"
+    exit 1
+fi
+
+#Create EKS cluster
+./rctl create cluster eks -f $REPO_NAME/$CLUSTER_SPEC
+#./rctl apply -f $REPO_NAME/$CLUSTER_SPEC
+if [ $? -eq 0 ];
+then
+    echo "[+] Successfully Created EKS cluster: $CLUSTER_NAME"
+else
+    echo "[+] Failed to create EKS cluster: $CLUSTER_NAME"
+    exit 1
+fi
+
+CLUSTER_STATUS_ITERATIONS=1
+CLUSTER_HEALTH_ITERATIONS=1
+CLUSTER_STATUS=`./rctl get cluster ${CLUSTER_NAME} -o json |jq '.status'|cut -d'"' -f2`
+while [ "$CLUSTER_STATUS" != "READY" ]
+do
+  sleep 60
+  if [ $CLUSTER_STATUS_ITERATIONS -ge 50 ];
+  then
+    break
+  fi
+  CLUSTER_STATUS_ITERATIONS=$((CLUSTER_STATUS_ITERATIONS+1))
+  CLUSTER_STATUS=`./rctl get cluster ${CLUSTER_NAME} -o json |jq '.status'|cut -d'"' -f2`
+  if [ $CLUSTER_STATUS == "PROVISION_FAILED" ];
+  then
+    echo -e " !! Cluster provision failed !!  "
+    echo -e " !! Exiting !!  " && exit -1
+  fi
+
+  PROVISION_STATUS=`./rctl get cluster ${CLUSTER_NAME} -o json |jq '.provision.status' |cut -d'"' -f2`
+
+  if [ $PROVISION_STATUS == "INFRA_CREATION_FAILED" ];
+  then
+    echo -e " !! Cluster provision failed !!  "
+    echo -e " !! Exiting !!  " && exit -1
+  fi
+
+  if [ $PROVISION_STATUS == "BOOTSTRAP_CREATION_FAILED" ];
+  then
+    echo -e " !! Cluster provision failed !!  "
+    echo -e " !! Exiting !!  " && exit -1
+  fi
+
+  PROVISION_STATE=`./rctl get cluster ${CLUSTER_NAME} -o json | jq '.provision.running_state' |cut -d'"' -f2`
+
+  echo "$PROVISION_STATE in progress"
+done
+if [ $CLUSTER_STATUS != "READY" ];
+then
+    echo -e " !! Cluster provision failed !!  "
+    echo -e " !! Exiting !!  " && exit -1
+fi
+if [ $CLUSTER_STATUS == "READY" ];
+then
+    echo "[+] Cluster Provisioned Successfully waiting for it to be healthy"
+    CLUSTER_HEALTH=`./rctl get cluster ${CLUSTER_NAME} -o json | jq '.health' |cut -d'"' -f2`
+    while [ "$CLUSTER_HEALTH" != 1 ]
+    do
+      echo "Iteration-${CLUSTER_HEALTH_ITERATIONS} : Waiting 60 seconds for cluster to be healthy..."
+      sleep 60
+      if [ $CLUSTER_HEALTH_ITERATIONS -ge 15 ];
+      then
+        break
+      fi
+      CLUSTER_HEALTH_ITERATIONS=$((CLUSTER_HEALTH_ITERATIONS+1))
+      CLUSTER_HEALTH=`./rctl get cluster ${CLUSTER_NAME} -o json | jq '.health' |cut -d'"' -f2`
+    done
+fi
+
+if [[ $CLUSTER_HEALTH == 0 ]];
+then
+    echo -e " !! Cluster is not healthy !!  "
+    echo -e " !! Exiting !!  " && exit -1
+fi
+if [[ $CLUSTER_HEALTH == 1 ]];
+then
+    echo "[+] Cluster Provisioned Successfully and is Healthy"
+fi
