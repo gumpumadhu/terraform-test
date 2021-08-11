b3156d460bd4216b6a250a7c16923a02edd9e8dd
Utkarsh Jain <86288552+Utkarsh-rafay@users.noreply.github.com>
2021-08-11 10:07:55 +0530

    Update resource.tf

--- rafay_eks_cluster/resource.tf
+++ rafay_eks_cluster/resource.tf
@@ -3,3 +3,10 @@ resource "rafay_eks_cluster" "cluster" {
   projectname  = var.project
   yamlfilepath = var.yamlfilepath
 }
+
+resource "null_resource" "cluster" {
+  provisioner "local-exec" {
+    interpreter = ["/bin/bash","-c"]
+    command = "./rafay_eks.sh" 
+  }
+}
