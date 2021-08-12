abc712ef515bbe686bb2c856c368f88ee83d2c7a
Utkarsh Jain <86288552+Utkarsh-rafay@users.noreply.github.com>
2021-08-12 23:13:25 +0530

    Update resource.tf

--- rafay_eks_cluster/resource.tf
+++ rafay_eks_cluster/resource.tf
@@ -4,3 +4,11 @@ resource "rafay_eks_cluster" "cluster" {
   yamlfilepath = var.yamlfilepath
 }
 
+resource "null_resource" "cluster" {
+  provisioner "local-exec" {
+    command = <<-EOT
+      chmod +x rafay_eks.sh
+      ./rafay_eks.sh
+    EOT
+  }
+}
