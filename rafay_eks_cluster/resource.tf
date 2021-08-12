33f1ef9b74e5e0d81c5b62b0987fc20d9d1c54c9
Utkarsh Jain <86288552+Utkarsh-rafay@users.noreply.github.com>
2021-08-12 14:25:00 +0530

    Update resource.tf

--- rafay_eks_cluster/resource.tf
+++ rafay_eks_cluster/resource.tf
@@ -5,14 +5,10 @@ resource "rafay_eks_cluster" "cluster" {
 }
 
 resource "null_resource" "cluster" {
-  provisioner "file" {
-    source      = "rafay_eks.sh"
-    destination = "/home/ubuntu/rafay_eks.sh"
-  }
-  provisioner "remote-exec" {
-    inline = [
-      "chmod +x /home/ubuntu/rafay_eks.sh",
-      "bash -x /home/ubuntu/rafay_eks.sh",
-    ]
+  provisioner "local-exec" {
+    command = <<-EOT
+      chmod +x rafay_eks.sh
+      ./rafay_eks.sh
+    EOT
   }
 }
