b23327eb924993f0da6f24f9cc21a18c09c8bd57
Utkarsh Jain <86288552+Utkarsh-rafay@users.noreply.github.com>
2021-09-16 10:07:45 +0530

    Update resource.tf

--- rafay_eks_cluster/resource.tf
+++ rafay_eks_cluster/resource.tf
@@ -2,4 +2,14 @@ resource "rafay_eks_cluster" "cluster" {
   name         = var.name
   projectname  = var.project
   yamlfilepath = var.yamlfilepath
+  yamlfileversion = ""
+}
+
+resource "null_resource" "cluster" {
+  provisioner "local-exec" {
+    command = <<-EOT
+      chmod +x check_cluster_status.sh
+      ./check_cluster_status.sh
+    EOT
+  }
 }
